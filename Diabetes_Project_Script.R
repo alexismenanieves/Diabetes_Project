85b49cdf71ccfc25b8027be9bdae169a9e779b7f
TIXM <alexis@TIXMs-iMac.local>
2020-06-23 18:42:55 -0500

    Last edition

--- Diabetes_Project_Script.R
+++ Diabetes_Project_Script.R
@@ -107,7 +107,11 @@ final_model <- predict(model_KNN, newdata = validation_set)
 confusionMatrix(final_model, validation_set$Diabetes)$overall[["Accuracy"]]
 
 # Step 5. Create an ensemble model and select the best model --------------
+
+# Define the models to use in the ensemble
 models <- c("knn","rpart","rf","gamLoess","gbm","svmLinear")
+
+# Define the tuning parameters
 knn_tg = data.frame(k = seq(3,50,1))
 rpart_tg = data.frame(cp = seq(0,0.1,len = 50))
 rf_tg = data.frame(mtry = seq(1,8,1))
@@ -117,6 +121,7 @@ gbm_tg = expand.grid(n.trees = c(100, 1000, 10000),
                      n.minobsinnode = 1)
 svmLinear_tg = data.frame(C = c(0.01, 0.05, 0.1, seq(0.25, 2, 0.25)))
 
+# Train each model
 suppressMessages(fits <- lapply(models, function(model){ 
   print(model)
   if(model == "gbm"){
@@ -140,19 +145,24 @@ suppressMessages(fits <- lapply(models, function(model){
   }
 }))
 
+# Give the names for each model
 names(fits) <- models
 
+# Calculate the ensemble prediction
 pred <- sapply(fits, function(object) 
   predict(object, newdata = validation_set))
-dim(pred)
 
+# Calculate the mean accuracy
 acc <- colMeans(pred == validation_set$Diabetes)
 acc
 mean(acc)
+
+# Use the majority vote to calculate the ensemble accuracy on the validation set
 votes <- rowMeans(pred == "Yes")
 y_hat <- ifelse(votes > 0.5, "Yes", "No")
 mean(y_hat == validation_set$Diabetes)
 
+# Select the best methods using the validation set
 ind <- acc >= mean(acc)
 best_votes <- rowMeans(pred[,ind]=="Yes")
 best_y_hat <- ifelse(best_votes > 0.5, "Yes", "No")
@@ -160,9 +170,19 @@ mean(best_y_hat == validation_set$Diabetes)
 
 # Step 6. Evaluate the best ensemble model on the test dataset ------------
 
+# Convert zeros to NA as we did in the train dataset
+test_set[,c(2:6)] <- apply(test_set[,c(2:6)], 2, function(x) {ifelse(x==0, NA, x)})
+
+# Let's apply the preprocess step on the test set
 test_set <- predict(preProcess_data, newdata = test_set)
+
+# Get the predictions for the test set
 test_pred <- sapply(fits, function(object) 
   predict(object, newdata = test_set))
+
+# By using the tuned index calculate the test prediction
 test_votes <- rowMeans(test_pred[,ind]=="Yes")
 test_y_hat <- ifelse(test_votes > 0.5, "Yes", "No")
+
+# Calculate the accuracy of this ensemble on the test set
 mean(test_y_hat == test_set$Diabetes)
